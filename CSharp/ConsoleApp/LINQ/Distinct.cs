48d71cf1d99ad5c1f85cd12a369bc858b30fc948
Anh Bui <anh.bui@pinewood.co.uk>
2022-06-01 17:50:41 +0100

    Test distinct with one field

---
+++ CSharp/ConsoleApp/LINQ/Distinct.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.LINQ
+{
+    internal class Distinct
+    {
+        internal void TestDinstinctByOneField()
+        {
+            List<TestData> _TestDataSet = new()
+            {
+                new TestData { Description = "A", ModelCode = "1"},
+                new TestData { Description = "A", ModelCode = "2"},
+                new TestData { Description = "B", ModelCode = "1"},
+                new TestData { Description = "C", ModelCode = "2"},
+            };
+
+            var _DistinctList = _TestDataSet.GroupBy(x => x.Description)
+                .Select(x => x.FirstOrDefault()).ToList();
+        }
+    }
+
+    internal class TestData
+    {
+        public string Description { get; set; }
+        public string ModelCode { get; set; }
+    }
+}
--- CSharp/ConsoleApp/Program.cs
+++ CSharp/ConsoleApp/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp.LINQ;
 using ConsoleApp.OOP.Inheritance;
 
 Console.WriteLine("Hello, World!");
 
+Distinct _Distinct = new();
+_Distinct.TestDinstinctByOneField();
